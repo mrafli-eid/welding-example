8932034923de3fb8f148dd77b261a099c1f894b3
daffaharizaleid <daffaharizal.eid@gmail.com>
2023-09-11 11:45:57 +0700

    Interval update

--- src/app/core/consts/app.const.ts
+++ src/app/core/consts/app.const.ts
@@ -1,1 +1,5 @@
 export const DEFAULT_INTERVAL = 30 * 60 * 1000;
+export const ONE_MINUTE_INTERVAL = 1 * 60 * 1000;
+export const TWO_MINUTE_INTERVAL = 2 * 60 * 1000;
+export const THREE_MINUTE_INTERVAL = 3 * 60 * 1000;
+export const HALF_MINUTE_INTERVAL = 30 * 1000;
