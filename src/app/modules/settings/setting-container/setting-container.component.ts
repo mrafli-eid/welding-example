5cc493f2b87f506c15ec27e52c231a4c9c932760
daffaharizaleid <daffaharizal.eid@gmail.com>
2023-09-15 16:51:05 +0700

    Fix: Bug setting fetching when save on click

--- src/app/modules/settings/setting-container/setting-container.component.ts
+++ src/app/modules/settings/setting-container/setting-container.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Breadcrumb } from "../../../core/models/breadcrumbs.model";
+import { SettingListComponent } from '../setting-list/setting-list.component';
+import { Setting } from 'src/app/core/models/setting.model';
 
 @Component({
     selector: 'ahm-setting-container',
@@ -10,4 +12,13 @@ export class SettingContainerComponent {
     breadcrumbs: Breadcrumb[] = [
         { label: 'Setting', link: '/settings' },
     ];
+    data: Setting[];
+
+
+    @ViewChild(SettingListComponent) settingList: SettingListComponent;
+
+    onFinished() {
+        this.data = null;
+        this.settingList.refreshData();
+    }
 }
