5c98d3428a642cf90e7bac6132629df7e99cb4b1
daffaharizaleid <daffaharizal.eid@gmail.com>
2023-09-21 09:02:52 +0700

    Update downloadFile function

--- src/app/core/helpers/http.helper.ts
+++ src/app/core/helpers/http.helper.ts
@@ -1,8 +1,9 @@
-export function downLoadFile(data: any, type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
+export function downLoadFile(data: any, fileName = 'excel.xlsx') {
+    const type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
     let blob = new Blob([ data ], { type: type });
     let url = window.URL.createObjectURL(blob);
-    let pwa = window.open(url);
-    if (!pwa || pwa.closed || typeof pwa.closed == 'undefined') {
-        alert('Please disable your Pop-up blocker and try again.');
-    }
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    a.click();
 }
