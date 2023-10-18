e9ecac085ed8bd39e645b6bac2d3915ec49d5c7e
daffaharizaleid <daffaharizal.eid@gmail.com>
2023-10-18 10:03:12 +0700

    Update user management

--- src/app/modules/user-management/permissions/permissions-list/permissions-list.dummy.ts
+++ src/app/modules/user-management/permissions/permissions-list/permissions-list.dummy.ts
@@ -43,12 +43,27 @@ export const DUMMY_PERMISSIONS_LIST = [
 
 export const DUMMY_ROLE_LIST = [
     {
-        role: 'Basic',
+        "id": "0835ed4c-bed3-4254-ada9-0cf23c6adff2",
+        "name": "staff"
     },
     {
-        role: 'Admin',
+        "id": "38e74d49-50ed-45ce-bff0-613aaecf3094",
+        "name": "Admin"
     },
     {
-        role: 'SuperAdmin',
+        "id": "8a367adb-877e-41b3-93c2-db32d0ca6dd6",
+        "name": "Manager"
     },
+    {
+        "id": "978141fa-c789-437a-b988-7713938f0030",
+        "name": "Basic"
+    },
+    {
+        "id": "f5f6ca32-ffe9-4d99-a32f-ff0d0f592d41",
+        "name": "SuperAdmin"
+    },
+    {
+        "id": "fefb41e2-b8ff-4cf2-ac25-af7e6f59e1c9",
+        "name": "Sales"
+    }
 ];
