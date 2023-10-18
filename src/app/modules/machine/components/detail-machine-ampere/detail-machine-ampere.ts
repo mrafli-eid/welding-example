98080437e5447dc02b92283e046db7ee32b6729d
Luckyeid <127484597+Luckyeid@users.noreply.github.com>
2023-10-18 16:13:46 +0700

    dummy up
    dummy up

--- src/app/modules/machine/components/detail-machine-ampere/detail-machine-ampere.ts
+++ src/app/modules/machine/components/detail-machine-ampere/detail-machine-ampere.ts
@@ -12,24 +12,222 @@ export const DUMMY_DETAIL_MACHINE_AMPERE: DetailMachineAmpereAndVoltage = {
             },
             {
                 "actual": 200.5
+            },
+            {
+                "actual": 0
+            },
+            {
+                "actual": 201.5
+            },
+            {
+                "actual": 200.5
+            },
+            {
+                "actual": 204.5
+            },
+            {
+                "actual": 200.5
+            },
+            {
+                "actual": 0
+            },
+            {
+                "actual": 190.5
+            },
+            {
+                "actual": 202.5
+            },
+            {
+                "actual": 206.5
+            },
+            {
+                "actual": 0
+            },
+            {
+                "actual": 197.5
+            },
+            {
+                "actual": 190.3
+            },
+            {
+                "actual": 199.5
+            },
+            {
+                "actual": 209.2
+            },
+            {
+                "actual": 0
+            },
+            {
+                "actual": 193.5
+            },
+            {
+                "actual": 200.5
+            },
+            {
+                "actual": 201.5
             }
         ],
         "data_setting": [
             {
-                "setting": 203.2
+                "setting": 209.2
+            },
+            {
+                "setting": 210.1
+            },
+            {
+                "setting": 0
+            },
+            {
+                "setting": 199.1
+            },
+            {
+                "setting": 205.1
+            },
+            {
+                "setting": 208.1
+            },
+            {
+                "setting": 203.7
+            },
+            {
+                "setting": 0
             },
             {
                 "setting": 200.1
+            },
+            {
+                "setting": 200.7
+            },
+            {
+                "setting": 204.1
+            },
+            {
+                "setting": 201.1
+            },
+            {
+                "setting": 0
+            },
+            {
+                "setting": 202.1
+            },
+            {
+                "setting": 203.1
+            },
+            {
+                "setting": 200.1
+            },
+            {
+                "setting": 201.1
+            },
+            {
+                "setting": 0
+            },
+            {
+                "setting": 190.7
+            },
+            {
+                "setting": 199.1
             }
         ],
         "data_label": [
-            {
-                "label": "12:44",
+             {
+                "label": "10:05",
                 "message": null,
                 "date_time": "2023-08-09 12:44:15"
             },
             {
-                "label": "12:44",
+                "label": "10:05",
+                "message": null,
+                "date_time": "2023-08-09 12:44:16"
+            },
+            {
+                "label": "10:05",
+                "message": null,
+                "date_time": "2023-08-09 12:44:16"
+            },
+            {
+                "label": "10:05",
+                "message": null,
+                "date_time": "2023-08-09 12:44:16"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 12:44:16"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 12:44:16"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 12:44:16"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 12:44:16"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 12:44:16"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 12:44:16"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 12:44:16"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 12:44:16"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 12:44:16"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 12:44:16"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 12:44:16"
+            },
+            {
+                "label": "10:07",
+                "message": null,
+                "date_time": "2023-08-09 12:44:16"
+            },
+            {
+                "label": "10:07",
+                "message": null,
+                "date_time": "2023-08-09 12:44:16"
+            },
+            {
+                "label": "10:07",
+                "message": null,
+                "date_time": "2023-08-09 12:44:16"
+            },
+            {
+                "label": "10:07",
+                "message": null,
+                "date_time": "2023-08-09 12:44:16"
+            },
+            {
+                "label": "10:07",
                 "message": null,
                 "date_time": "2023-08-09 12:44:16"
             }
--- src/app/modules/machine/components/detail-machine-voltage/detail-machine-voltage.ts
+++ src/app/modules/machine/components/detail-machine-voltage/detail-machine-voltage.ts
@@ -8,28 +8,226 @@ export const DUMMY_DETAIL_MACHINE_VOLTAGE: DetailMachineAmpereAndVoltage = {
         "maximum": null,
         "data_actual": [
             {
-                "actual": 19
+                "actual": 20.4
             },
             {
-                "actual": 19
+                "actual": 19.5
+            },
+            {
+                "actual": 20.1
+            },
+            {
+                "actual": 22
+            },
+            {
+                "actual": 20.3
+            },
+            {
+                "actual": 0
+            },
+            {
+                "actual": 19.8
+            },
+            {
+                "actual": 20.4
+            },
+            {
+                "actual": 22.1
+            },
+            {
+                "actual": 20.3
+            },
+            {
+                "actual": 0
+            },
+            {
+                "actual": 19.4
+            },
+            {
+                "actual": 19.9
+            },
+            {
+                "actual": 22.1
+            },
+            {
+                "actual": 19.1
+            },
+            {
+                "actual": 18.1
+            },
+            {
+                "actual": 0
+            },
+            {
+                "actual": 19.4
+            },
+            {
+                "actual": 19.9
+            },
+            {
+                "actual": 19.1
             }
         ],
         "data_setting": [
             {
-                "setting": 14
+                "setting": 19.3
             },
             {
-                "setting": 14
+                "setting": 20.2
+            },
+            {
+                "setting": 20.2
+            },
+            {
+                "setting": 20.9
+            },
+            {
+                "setting": 22
+            },
+            {
+                "setting": 0
+            },
+            {
+                "setting": 22.2
+            },
+            {
+                "setting": 29.2
+            },
+            {
+                "setting": 21.2
+            },
+            {
+                "setting": 26.2
+            },
+            {
+                "setting": 21.2
+            },
+            {
+                "setting": 22.2
+            },
+            {
+                "setting": 0
+            },
+            {
+                "setting": 22.2
+            },
+            {
+                "setting": 20.2
+            },
+            {
+                "setting": 19.2
+            },
+            {
+                "setting": 18.2
+            },
+            {
+                "setting": 0
+            },
+            {
+                "setting": 20.2
+            },
+            {
+                "setting": 22.2
             }
         ],
         "data_label": [
             {
-                "label": "20:26",
+                "label": "10:05",
                 "message": null,
                 "date_time": "2023-08-09 20:26:55"
             },
             {
-                "label": "20:27",
+                "label": "10:05",
+                "message": null,
+                "date_time": "2023-08-09 20:27:57"
+            },
+            {
+                "label": "10:05",
+                "message": null,
+                "date_time": "2023-08-09 20:27:57"
+            },
+            {
+                "label": "10:05",
+                "message": null,
+                "date_time": "2023-08-09 20:27:57"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 20:27:57"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 20:27:57"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 20:27:57"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 20:27:57"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 20:27:57"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 20:27:57"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 20:27:57"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 20:27:57"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 20:27:57"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 20:27:57"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 20:27:57"
+            },
+            {
+                "label": "10:06",
+                "message": null,
+                "date_time": "2023-08-09 20:27:57"
+            },
+            {
+                "label": "10:07",
+                "message": null,
+                "date_time": "2023-08-09 20:27:57"
+            },
+            {
+                "label": "10:07",
+                "message": null,
+                "date_time": "2023-08-09 20:27:57"
+            },
+            {
+                "label": "10:07",
+                "message": null,
+                "date_time": "2023-08-09 20:27:57"
+            },
+            {
+                "label": "10:07",
                 "message": null,
                 "date_time": "2023-08-09 20:27:57"
             }
