454c0ae0ab47cedd88bb2e2cea3c13b9c075bc60
daffaharizaleid <daffaharizal.eid@gmail.com>
2023-10-19 13:51:04 +0700

    Update notification

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -24,22 +24,26 @@ export class NavbarComponent implements OnInit {
     constructor(
         private sidebarService: SidebarService,
         private notificationService: NotificationService,
-        private machineService: MachineService,
         private router: Router,
         private elRef: ElementRef
     ) {}
 
-    listNotif: notification[] = NOTIF_DUMMY.filter((data) => data.status !== true);
+    listNotif: notification[] = NOTIF_DUMMY.filter(
+        (data) => data.status !== true
+    );
     params = {
         start: null,
         end: null,
-        type: 'default'
-    }
+        type: 'default',
+    };
 
     isDropdownOpen = false;
 
     toggleDropdown() {
         this.isDropdownOpen = !this.isDropdownOpen;
+        if (this.isDropdownOpen == true) {
+            this.getListNotif();
+        }
     }
 
     @HostListener('document:click', ['$event'])
@@ -53,78 +57,9 @@ export class NavbarComponent implements OnInit {
         setInterval(() => {
             this.todaysDate = new Date();
         }, 1000);
-
-        this.temperatureMirror();
-        this.dewPoint();
-        this.rurgeCell();
-        this.sansoMatic();
-        this.rpmSpindle();
-        this.servoLoad();
-        this.runningHour();
-
-        setInterval(() => {
-            this.temperatureMirror()
-            this.dewPoint();
-            this.rurgeCell();
-            this.sansoMatic();
-            this.rpmSpindle();
-            this.servoLoad();
-            this.runningHour();
-        }, TEN_MINUTE_INTERVAL);
-
-        setInterval(() => {
-            this.getListNotif();
-        }, TEN_SECOND_INTERVAL);
-
     }
 
-    temperatureMirror() {
-        this.machineService.getTemperatureMirror('LASER', this.params)
-        .pipe(take(1))
-            .subscribe();
-    }
-    dewPoint() {
-        this.machineService.getDewPoint('LASER', this.params)
-        .pipe(take(1))
-        .subscribe();
-    }
-    rurgeCell() {
-        this.machineService.getRurgeCell('LASER', this.params)
-        .pipe(take(1))
-        .subscribe();
-    }
-    sansoMatic() {
-        this.machineService.getSansoMatic('BORING', this.params)
-        .pipe(take(1))
-        .subscribe();
-    }
-    rpmSpindle() {
-        this.machineService.getRpmSpindle('BORING', this.params)
-        .pipe(take(1))
-        .subscribe();
-    }
-    servoLoad() {
-        ROBOT_PARAMS.map((item) => {
-            this.machineService.getServoLoad(
-                item.machine_name,
-                item.robot_name,
-                this.params
-            )
-            .pipe(take(1))
-            .subscribe();
-        });
-    }
-    runningHour() {
-        ROBOT_PARAMS.map((item) => {
-            this.machineService.getRunningHour(
-                item.machine_name,
-                item.robot_name,
-                this.params
-            )
-            .pipe(take(1))
-            .subscribe();
-        });
-    }
+  
 
     toggle() {
         this.sidebarService.toggle();
@@ -135,7 +70,7 @@ export class NavbarComponent implements OnInit {
             .updateNotification(notifId)
             .pipe(take(1))
             .subscribe(() => {
-                this.getListNotif()
+                this.getListNotif();
             });
     }
 
@@ -146,7 +81,7 @@ export class NavbarComponent implements OnInit {
             .subscribe({
                 next: (response) => {
                     this.listNotif = response.data.filter(
-                        (data:notification) => data.status !== true
+                        (data: notification) => data.status !== true
                     );
                 },
             });
