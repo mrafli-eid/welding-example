285d9e0d9688f11667e745a48bbd0e3ad549d3b6
daffaharizaleid <daffaharizal.eid@gmail.com>
2023-10-30 08:07:25 +0700

    Feat: Login integration

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -9,7 +9,7 @@ import { UserService } from '../../core/services/user.service';
     styleUrls: [ './login.component.scss' ],
 })
 export class LoginComponent {
-    private formGroup: FormGroup = new FormGroup({
+    formGroup: FormGroup = new FormGroup({
         email: new FormControl('', [ Validators.required, Validators.email ]),
         password: new FormControl('', [ Validators.required ]),
     });
@@ -20,17 +20,18 @@ export class LoginComponent {
 
     login() {
         this.router.navigate([ '/dashboard' ]);
-        // this.formGroup.markAllAsTouched();
-        // if (this.formGroup.valid) {
-        //     const body = this.formGroup.value;
-        //     this.userService.login(body).subscribe({
-        //         next: () => {
-        //             this.router.navigate(['/dashboard']);
-        //         },
-        //         error: () => {
-        //             this.router.navigate(['/dashboard']);
-        //         }
-        //     });
-        // }
+        this.formGroup.markAllAsTouched();
+        if (this.formGroup.valid) {
+            const body = this.formGroup.value;
+            console.log(body);
+            this.userService.login(body).subscribe({
+                next: () => {
+                    this.router.navigate(['/dashboard']);
+                },
+                error: () => {
+                    this.router.navigate(['/dashboard']);
+                }
+            });
+        }
     }
 }
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -18,6 +19,7 @@ import { LoginComponent } from './login.component';
         MatButtonModule,
         MatInputModule,
         MatFormFieldModule,
+        ReactiveFormsModule
     ],
 })
 export class LoginModule {
